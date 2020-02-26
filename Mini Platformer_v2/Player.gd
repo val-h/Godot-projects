05eaf79379c8ae6780039df4d7ea322032079268
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-02-26 04:34:18 +0000

    Charecter movement update
    Falling from ledges
    fallling animation
    friction in air and on ground
    acceleration

--- Mini Platformer_v2/Player.gd
+++ Mini Platformer_v2/Player.gd
@@ -3,30 +3,40 @@ extends KinematicBody2D
 # Variables
 const UP = Vector2(0, -1)
 const GRAVITY = 20
-const SPEED = 200
+const ACCELERATION = 20
+const MAX_SPEED = 320
 const JUMP_HEIGHT = -550
 var motion = Vector2()
+var friction = false
 
 func _physics_process(delta):
 	motion.y += GRAVITY
+	friction = false
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
 		get_node("Player_sprite").flip_h = false	# get_node("Player_sprite")   and   $Player_sprite    work the same way until we have multiple sprites. then use get_node
 		$Player_sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
 		$Player_sprite.flip_h = true
 		$Player_sprite.play("Run")
 	else:
 		$Player_sprite.play("Idle")
-		motion.x = 0
+		friction = true
 	
 	if is_on_floor():
-		if Input.is_action_pressed("ui_up"):
+		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.15)
 	else:
-		$Player_sprite.play("Jump")
+		if motion.y < 0:
+			$Player_sprite.play("Jump")
+		else:
+			$Player_sprite.play('Fall')
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.035)
 	
 	motion = move_and_slide(motion, UP)
 	
